29ef8f645b4239f37747289547920487c663aa91
Henri Grant <mhggoulet@gmail.com>
2023-01-22 16:25:37 -0500

    test

---
+++ src/hello_world.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include
+using namespace std;
+
+
+int main() {
+	int number;
+	number = 81;
+	if (number == 5)
+	{
+		cout << "the number is: " << number << endl;
+		return 0;
+
+	}
+	else
+	{
+		cout << "the number ain't 5, its: " << number << endl;
+		return 1;
+	}
+}
